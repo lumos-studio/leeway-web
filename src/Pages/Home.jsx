1dcc2ad151042edc0b72526fb0bbca20be851544
Rishiraj <rishirajt21@gmail.com>
2023-05-25 13:51:53 +0530

    transaction list UI

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,11 +8,20 @@ function Home() {
     return (
         <div class="home">
             <h1>leeway</h1>
-            <div class="ip">
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
-            </div>
+            <section class="data">
+                <div class="data1">
+                    <p>500 ₹</p>
+                    <p>105.43₹/l</p>
+                    <p>4.3 Ltr</p>
+                    <p>12/02/2023 - 4:21 PM(42,312KM)</p>
+                </div>
+                <div class="data2">
+                    <p class="rupees">500 ₹</p>
+                    <p class="rupeesl">105.43₹/l</p>
+                    <p class="ltr">4.3 Ltr</p>
+                    <p class="date">12/02/2023 - 4:21 PM(42,312KM)</p>
+                </div>
+            </section>
             <div className="btn"><button className="add" onClick={handleClick}><img src="addbutton.svg"></img></button></div>
         </div >
 
