a090321f446795b118ff472b5c138a46ada23fcc
RISHIRAJ <75069505+rishirajt11@users.noreply.github.com>
2023-06-01 16:23:12 +0530

    local storage (#11)
    * local storage
    
    * suggested changes in local storage
    
    * changes in local storage using JSON.stringify function

--- src/Pages/Transaction.jsx
+++ src/Pages/Transaction.jsx
@@ -3,13 +3,27 @@ import React, { useState } from 'react';
 function Transaction() {
 
     const navigate = useNavigate();
-    const [km, setkm] = useState('');
+    const [km, setKm] = useState('');
+    const [amount, setAmount] = useState('');
+    const [unitPrice, setUnitPrice] = useState('');
+    const [litres, setLitres] = useState('');
+    const [date, setDate] = useState('');
 
-    const handleInputChange = (event) => {
-        setkm(event.target.value);
+    const handleInputChange = (event, setter) => {
+        setter(event.target.value);
     };
     const handleTickClick = () => {
-        console.log('Data entered:', km);
+        if (km.trim() !== '' && amount.trim() !== '' && unitPrice.trim() !== '' && litres.trim() !== '' && date.trim() !== '') {
+            const transactionData = {
+                km,
+                amount,
+                unitPrice,
+                litres,
+                date
+            };
+            localStorage.setItem('transaction', JSON.stringify(transactionData));
+            console.log('Data entered:', transactionData);
+        }
     };
 
     function handleClick() {
@@ -21,19 +35,16 @@ function Transaction() {
         <div class="next">
             <h1>fuel expense</h1>
             <div class="transaction-ip">
-                <input type="number" placeholder="current km" value={km} onChange={handleInputChange} />
-
-                <input type="number" placeholder="amount" />
-                <input type="number" placeholder="unit price" />
-                <input type="number" placeholder="litres" />
-                <input type="date" placeholder="date" />
+                <input type="number" placeholder="current km" value={km} onChange={(e) => handleInputChange(e, setKm)} />
+                <input type="number" placeholder="amount" value={amount} onChange={(e) => handleInputChange(e, setAmount)} />
+                <input type="number" placeholder="unit price" value={unitPrice} onChange={(e) => handleInputChange(e, setUnitPrice)} />
+                <input type="number" placeholder="litres" value={litres} onChange={(e) => handleInputChange(e, setLitres)} />
+                <input type="date" placeholder="date" value={date} onChange={(e) => handleInputChange(e, setDate)} />
             </div>
             <div className="tickbtn"><button class="tick" onClick={handleTickClick}><img src="savebutton.svg"></img></button></div>
         </div>
     )
 }
-
-
 export default Transaction
 
 
