316a4a6e0c13e87bf403a33862bff9da5f039d9c
Rishiraj <rishirajt21@gmail.com>
2023-06-08 21:00:59 +0530

    added back navigation button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,23 +8,6 @@ function Home() {
         const storedTransactions = JSON.parse(localStorage.getItem('transactions')) || [];
         setTransactions(storedTransactions);
     }, []);
-    // const data = [
-    //     {
-    //         id: 1,
-    //         amount: '500 ₹',
-    //         amountperlitre: '105.43₹/l',
-    //         litres: '4.3 Ltr',
-    //         date: '12/02/2023 - 4:21 PM(42,312KM)',
-    //     },
-    //     {
-    //         id: 2,
-    //         amount: '600 ₹',
-    //         amountperlitre: '105.43₹/l',
-    //         litres: '4.3 Ltr',
-    //         date: '12/02/2023 - 4:21 PM(42,312KM)',
-    //     },
-    // ];
-
     function handleClick() {
         navigate("/transaction")
     }
@@ -32,8 +15,6 @@ function Home() {
         <div class="home">
             <h1>leeway</h1>
             <div className="data">
-                {/* {data.map(item => (
-                    <div key={item.id} className="list"> */}
                 {transactions.map((item, index) => (
                     <div key={index} className="list">
                         <div className="list-item rs">{item.amount}</div>
