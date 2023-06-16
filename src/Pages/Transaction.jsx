0360d030cf1e2ceadb80b287b0d15d3f10d08897
RISHIRAJ <75069505+rishirajt11@users.noreply.github.com>
2023-06-16 13:17:51 +0530

    Smart inputs (#23)
    * Smart inputs
    
    * changes in smart imput
    
    * made changes in smart input

--- src/Pages/Transaction.jsx
+++ src/Pages/Transaction.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 function Transaction() {
 
     const navigate = useNavigate();
@@ -12,6 +12,38 @@ function Transaction() {
     const handleInputChange = (event, setter) => {
         setter(event.target.value);
     };
+
+    const formatValue = (value) => {
+        if (!isNaN(value)) {
+            const roundedValue = parseFloat(value).toFixed(2);
+            return roundedValue;
+        }
+        return value;
+    };
+    const calculateLitresOrUnitPrice = () => {
+        if (amount && unitPrice) {
+            const calculatedLitres = amount / unitPrice;
+            setLitres(formatValue(calculatedLitres));
+        } else if (amount && litres) {
+            const calculatedUnitPrice = amount / litres;
+            setUnitPrice(formatValue(calculatedUnitPrice));
+        } else if (unitPrice && litres) {
+            const calculatedAmount = unitPrice * litres;
+            setAmount(formatValue(calculatedAmount));
+        }
+    };
+    const handleSecondField = () => {
+        calculateLitresOrUnitPrice();
+    };
+    const handleLitresChange = (event) => {
+        const newLitres = event.target.value;
+        setLitres(newLitres);
+        if (amount && newLitres) {
+            const calculatedUnitPrice = amount / newLitres;
+            setUnitPrice(formatValue(calculatedUnitPrice));
+        }
+    };
+
     const handleTickClick = () => {
         if (km.trim() !== '' && amount.trim() !== '' && unitPrice.trim() !== '' && litres.trim() !== '' && date.trim() !== '') {
             const transactionData = {
@@ -30,6 +62,7 @@ function Transaction() {
             setUnitPrice('');
             setLitres('');
             setDate('');
+            navigate("/home");
         }
     };
     const handleBackClick = () => {
@@ -41,9 +74,9 @@ function Transaction() {
             <h1><button className="back" onClick={handleBackClick}><img src="back.svg"></img></button>fuel expense</h1>
             <div class="transaction-ip">
                 <input type="number" placeholder="current km" value={km} onChange={(e) => handleInputChange(e, setKm)} />
-                <input type="number" placeholder="amount" value={amount} onChange={(e) => handleInputChange(e, setAmount)} />
-                <input type="number" placeholder="unit price" value={unitPrice} onChange={(e) => handleInputChange(e, setUnitPrice)} />
-                <input type="number" placeholder="litres" value={litres} onChange={(e) => handleInputChange(e, setLitres)} />
+                <input type="number" placeholder="amount" value={amount} onChange={(e) => handleInputChange(e, setAmount)} onBlur={handleSecondField} />
+                <input type="number" placeholder="unit price" value={unitPrice} onChange={(e) => handleInputChange(e, setUnitPrice)} onBlur={handleSecondField} />
+                <input type="number" placeholder="litres" value={litres} onChange={handleLitresChange} onBlur={handleSecondField} />
                 <input type="date" placeholder="date" value={date} onChange={(e) => handleInputChange(e, setDate)} />
             </div>
             <div className="tickbtn"><button class="tick" onClick={handleTickClick}><img src="savebutton.svg"></img></button></div>
@@ -51,7 +84,6 @@ function Transaction() {
     )
 }
 export default Transaction
-
 
 
 
