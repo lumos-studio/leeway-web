e5f0f2694b8f4316cbbb58bbce0b9644323aeaab
RISHIRAJ <75069505+rishirajt11@users.noreply.github.com>
2023-06-23 13:23:09 +0530

    Settings Page UI (#28)
    * added trsansaction page
    
    * made suggested changes.
    
    * changes

---
+++ src/Pages/settings.jsx
@@ -0,0 +1,45 @@
+import { useNavigate, Link } from 'react-router-dom';
+import React from 'react';
+
+function Settings() {
+    const navigate = useNavigate();
+
+    const handleBackClick = () => {
+        navigate("/home");
+    };
+
+
+    return (
+        <div className="settings">
+            <h1><button className="back" onClick={handleBackClick}><img src="back.svg"></img></button>settings</h1>
+            <section>
+                <div className='settings-option'>
+                    <div>distance unit</div>
+                    <div>kilo meter(km)</div>
+                </div>
+                <div className='settings-option'>
+                    <div>quantity unit</div>
+                    <div>litre(l)</div>
+                </div>
+                <div className='settings-option'>
+                    <div>currency</div>
+                    <div>rupee(₹)</div>
+                </div>
+            </section>
+            <div className='t-c-signout'>
+                <Link to='#' className='terms-conditions'>terms & conditions</Link>
+                <button className='signout'>sign out</button>
+            </div>
+        </div>
+    );
+}
+
+export default Settings;
+
+
+
+
+
+
+
+
